caf309c3ae79d261c7f5664bf7dc761805b20a45
KymRhys2@k22 <kymrhys@gmail.com>
2025-12-28 12:30:29 +0800

    finished: sine easing

---
+++ pages/01b_challenge/script.js
@@ -0,0 +1,10 @@
+gsap.to(
+  ".card",
+
+  {
+    boxShadow: "0 0 60px 20px rgba(84, 160, 255, 0.6)", // Glow effect
+    duration: 0.5,
+    repeat: -1,
+    yoyo: true,
+  }
+);
---
+++ pages/03a_power-easing/script.js
@@ -0,0 +1,7 @@
+import gsap from "gsap";
+
+gsap.to(".box", {
+  duration: 2.5,
+  ease: "bounce",
+  y: -500,
+});
